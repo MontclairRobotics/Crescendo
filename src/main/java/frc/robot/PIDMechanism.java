4883b19483d158e19d62a8048e4906ad593d4457
Voxel <jamesgormleyrechs@gmail.com>
2024-02-05 16:41:16 -0500

    Update PIDMechanism.java

--- src/main/java/frc/robot/PIDMechanism.java
+++ src/main/java/frc/robot/PIDMechanism.java
@@ -10,8 +10,7 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
 
-public class PIDMechanism implements Sendable
-{
+public class PIDMechanism implements Sendable {
     private PIDController pidController;
     private boolean usingPID;
     private double measurement;
@@ -20,8 +19,7 @@ public class PIDMechanism implements Sendable
     
     private boolean clampOutput = true;
 
-    public PIDMechanism(PIDController pidController)
-    {
+    public PIDMechanism(PIDController pidController) {
         this.pidController = pidController;
     }
 
@@ -30,18 +28,15 @@ public class PIDMechanism implements Sendable
      * 
      * @return PIDController for this mechanism
      */
-    public PIDController getPIDController()
-    {
+    public PIDController getPIDController() {
         return pidController;
     }
 
-    public void disableOutputClamping()
-    {
+    public void disableOutputClamping() {
         clampOutput = false;
     }
 
-    public double getTarget()
-    {
+    public double getTarget() {
         return pidController.getSetpoint();
     }
 
@@ -50,14 +45,12 @@ public class PIDMechanism implements Sendable
      * 
      * @param target
      */
-    public void setTarget(double target)
-    {
+    public void setTarget(double target) {
         pidController.reset();
         updateTarget(target);
     }
 
-    public void updateTarget(double target)
-    {
+    public void updateTarget(double target) {
         pidController.setSetpoint(target);
         usingPID = !pidController.atSetpoint();
     }
@@ -65,8 +58,7 @@ public class PIDMechanism implements Sendable
     /**
      * Stop using PID to determine speed
      */
-    public void cancel()
-    {
+    public void cancel() {
         usingPID = false;
     }
 
@@ -74,13 +66,11 @@ public class PIDMechanism implements Sendable
      * Set the measurement of where it currently is
      * @param measurement
      */
-    public void setMeasurement(double measurement)
-    {
+    public void setMeasurement(double measurement) {
         this.measurement = measurement;
     }
 
-    public double getMeasurement()
-    {
+    public double getMeasurement() {
         return measurement;
     }
 
@@ -88,8 +78,7 @@ public class PIDMechanism implements Sendable
      * Set the speed manually (without PID)
      * @param speed
      */
-    public void setSpeed(double speed) //duck you fylan
-    {
+    public void setSpeed(double speed) { // duck you fylan
         this.directSpeed = speed;
     }
     /**
@@ -100,20 +89,16 @@ public class PIDMechanism implements Sendable
      * <li>otherwise, use {@link PIDMechanism#setSpeed(double) setSpeed()} to set the speed manually
      * </ul>
      */
-    public void update()
-    {
-        if(pidController.atSetpoint())
-        {
+    public void update() {
+        if(pidController.atSetpoint()) {
             cancel();
         }
 
-        if(usingPID)
-        {
+        if(usingPID) {
             // Logging.info("USIND PID BITCHES");
             double calulation = pidController.calculate(measurement);
 
-            if(clampOutput)
-            {
+            if(clampOutput) {
                 //speed = Math555.clamp1(calulation);
                 //We are now clamping manually because fun fun fun
                 if(speed < -1) {
@@ -123,19 +108,15 @@ public class PIDMechanism implements Sendable
                     speed = 1.0;
                 }
             }
-            else 
-            {
+            else {
                 speed = calulation;
             }
-        }
-        else 
-        {
+        } else {
             // Logging.info("UNOT SIND PID BITCHES");
             speed = directSpeed;
         }
 
-        if(pidController.atSetpoint() && usingPID)
-        {
+        if(pidController.atSetpoint() && usingPID) {
             cancel();
         }
     }
@@ -143,8 +124,7 @@ public class PIDMechanism implements Sendable
      * get the speed determined by the {@link PIDMechanism#update() update method}
      * @return speed
      */
-    public double getSpeed() 
-    {
+    public double getSpeed() {
         return speed;
     }
 
@@ -156,18 +136,15 @@ public class PIDMechanism implements Sendable
      * </ul>
      * @return boolean
      */
-    public boolean active()
-    {
+    public boolean active() {
         return usingPID;
     }
-    public boolean free()
-    {
+    public boolean free() {
         return !usingPID;
     }
 
     @Override
-    public void initSendable(SendableBuilder builder) 
-    {
+    public void initSendable(SendableBuilder builder) {
         builder.addDoubleProperty ("P", pidController::getP, pidController::setP);
         builder.addDoubleProperty ("I", pidController::getI, pidController::setI);
         builder.addDoubleProperty ("D", pidController::getD, pidController::setD);
@@ -178,16 +155,13 @@ public class PIDMechanism implements Sendable
         builder.setSmartDashboardType("ShuffleboardLayout");
     }
 
-    public Command goToSetpoint(double target, Subsystem... requirements)
-    {
+    public Command goToSetpoint(double target, Subsystem... requirements) {
         return goToSetpoint(() -> target, requirements);
     }
 
-    public Command goToSetpoint(DoubleSupplier target, Subsystem... requirements)
-    {
+    public Command goToSetpoint(DoubleSupplier target, Subsystem... requirements) {
         /* Create an Anonymous Command cuz Command is an interface */
-        return new Command()
-        {
+        return new Command() {
             // How commands work approximately (not actually)
             // initialize();
             // while(!isFinished)
@@ -197,64 +171,32 @@ public class PIDMechanism implements Sendable
             // end();
 
             @Override
-            public Set<Subsystem> getRequirements() 
-            {
+            public Set<Subsystem> getRequirements() {
                 return Set.of(requirements);
             }
 
             @Override
-            public void initialize()
-            {
+            public void initialize() {
                 setTarget(target.getAsDouble());
             }
 
             @Override
-            public void execute()
-            {
+            public void execute() {
                 updateTarget(target.getAsDouble());
             }
 
             @Override
-            public boolean isFinished()
-            {
+            public boolean isFinished() {
                 return pidController.atSetpoint();
             }
 
             @Override
-            public void end(boolean inter)
-            {
+            public void end(boolean inter) {
                 PIDMechanism.this.cancel();
             }
         };
     }
 }
-
-
-/* 
-class MultiPIDMechanism
-{
-    PIDMechanism(Map<String, PIDController>) - constructor
-    PIDController controller(String name)    - get the controller with the specified name
-
-    void add(String name, PIDController) - add a new controller to this mechanism
-
-    void target(String name, double) - target measurement for controller
-    void cancel()                    - cancel all pidding
-
-    void setMeasurement(String name, double) - set measurement for the named pid controller
-    void setSpeed(double)                    - set speed directly. ignore if pidding
-
-    void update() - update speed
-
-    double get()     - get resultant speed
-    boolean active() - get if any pidding is occurring
-
-    String current() - get the name of the current controller, or 'null' if no pidding is ocurring
-}
-*/
-
-
-
 
 
 
