19c55d9eed9cc57d84fda3259a1968a4983fd7d8
Voxel <jamesgormleyrechs@gmail.com>
2024-02-05 22:58:43 -0500

    fixed spacing

--- src/main/java/frc/robot/animation3/Animations.java
+++ src/main/java/frc/robot/animation3/Animations.java
@@ -26,7 +26,7 @@ public class Animations {
 
     public static Runnable rainbowAnimation() {
         return () -> {
-            final int offset = (int)(LedConstants.BUFFER_SIZE * Timer.getFPGATimestamp());
+            final int offset = (int) (LedConstants.BUFFER_SIZE * Timer.getFPGATimestamp());
             for (int i = 0; i < LedConstants.BUFFER_SIZE; i++) {
                 int hue = ((offset + i) * 180 / LedConstants.BUFFER_SIZE) % 180;
                 RobotContainer.led.setHSV(i, hue, 255, 255);
