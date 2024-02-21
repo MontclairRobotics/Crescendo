734f751cab6d6fa6922ad0b4f382aafe809262c1
Voxel <jamesgormleyrechs@gmail.com>
2024-02-21 14:08:20 -0500

    made swerveDrive private

--- src/main/java/frc/robot/subsystems/Auto.java
+++ src/main/java/frc/robot/subsystems/Auto.java
@@ -28,10 +28,10 @@ public class Auto extends SubsystemBase {
 
     public void setupPathPlanner() {
         AutoBuilder.configureHolonomic(
-            RobotContainer.drivetrain.swerveDrive::getPose, // Robot pose supplier
-            RobotContainer.drivetrain.swerveDrive::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
-            RobotContainer.drivetrain.swerveDrive::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
-            RobotContainer.drivetrain.swerveDrive::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
+            RobotContainer.drivetrain.getSwerveDrive()::getPose, // Robot pose supplier
+            RobotContainer.drivetrain.getSwerveDrive()::resetOdometry, // Method to reset odometry (will be called if your auto has a starting pose)
+            RobotContainer.drivetrain.getSwerveDrive()::getRobotVelocity, // ChassisSpeeds supplier. MUST BE ROBOT RELATIVE
+            RobotContainer.drivetrain.getSwerveDrive()::setChassisSpeeds, // Method that will drive the robot given ROBOT RELATIVE ChassisSpeeds
             Constants.AutoConstants.PATH_FOLLOWER_CONFIG,
             () -> {
                 Optional<Alliance> alliance = DriverStation.getAlliance();
